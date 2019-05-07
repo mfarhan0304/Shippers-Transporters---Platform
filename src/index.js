b5e801b8701f09cef499a1314c242b8291b5e9d7
mfarhan0304 <mfarhan0304@gmail.com>
2019-05-07 18:32:31 +0700

    fix Date DataType

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
+const { GraphQLScalarType } = require('graphql')
+const { Kind } = require('graphql/language')
+
+const resolverMap = {
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    parseValue(value) {
+      return new Date(value); // value from the client
+    },
+    serialize(value) {
+      return value.getTime(); // value sent to the client
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return new Date(ast.value) // ast value is always in string format
+      }
+      return null;
+    },
+  }),
+};
 
 const resolvers = {
   Query: {
