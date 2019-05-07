9189c6b570df163392f8f1949d60ccf1a6b4a7e0
mfarhan0304 <mfarhan0304@gmail.com>
2019-05-07 12:48:56 +0700

    connect db

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
 
 const resolvers = {
   Query: {
-    jobs: () => jobs,
-    bids: () => bids,
-    bidsToJob: (parent, args) => postMessage.find(bid => bid.Job.id === args.id)
+    jobs(parent, args, ctx, info) {
+      return ctx.db.query.jobs({ }, info)
+    },
+    bids(parent, args, ctx, info) {
+      return ctx.db.query.bids({ }, info)
+    },
+    bidsToJob(parent, args, ctx, info) {
+      return ctx.db.query.bids({ where: {job_id: {id: args.job_id} } }, info)
+    },
   }
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: req => ({
+      ...req,
+      db: new Prisma({
+        typeDefs: 'src/generated/prisma.graphql',
+        endpoint: 'https://us1.prisma.sh/muhammad-farhan-f93c9b/database/dev',
+      }),
+  }),
 })
 
 server.start(() => console.log(`The server is running on http://localhost:4000`))
